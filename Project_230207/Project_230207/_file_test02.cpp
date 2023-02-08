101242779ea950bd29a910138cdce4d04e79a943
gnaYnahC <acy7454@office.skhu.ac.kr>
2023-02-08 14:32:36 +0900

    파일 입출력 실습 완료

--- Project_230207/Project_230207/_file_test02.cpp
+++ Project_230207/Project_230207/_file_test02.cpp
@@ -18,18 +18,44 @@ int main() {
     // member.txt. 에서 한 줄씩 "이름"과 "비밀번호"를 검사하여 로그인 성공시 "로그인 성공" 실패시 "로그인 실패" 출력
     string fileName, filePassword;
     ifstream inputFile("member.txt");
+    bool is_login = false; //로그인 성공 여부를 저장하는 변수
     while (inputFile >> fileName >> filePassword) {
         if (name == fileName && password == filePassword) {
-            cout << "로그인 성공" << endl;
-            return -1;
+            is_login = true;
+            break;
         }
     }
-    if (name != fileName || password != filePassword) {
-        cout << "로그인 실패" << endl;
-        return -2;
-    }
+    if (is_login) cout << "로그인 성공" ;
+    else cout << "로그인 실패";
 
-    inputFile.close();
-
-    return 0;
 }
+
+ 
+
+
+/* //다른 방법
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+    string name;
+    string password;
+    string line;
+    cout << "이름을 입력하세요: ";
+    cin >> name;
+    cout << "비밀번호를 입력하세요: ";
+    cin >> password;
+    ifstream file("member.txt");
+    string login = name + " " + password;
+    while (getline(file, line)) {
+        if (line == login) {
+            cout << "로그인 성공";
+            break;
+        }
+    }
+    if (line != login) {
+        cout << "로그인 실패";
+    }
+}
+*/
--- Project_230207/Project_230207/_file_test04.cpp
+++ Project_230207/Project_230207/_file_test04.cpp
@@ -1,67 +1,92 @@
+
+//-----------번외 실습(코딩 실패 ㅠ)--------------
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
-// input file stream -> ifstream
-// ouput file stream -> ofstream
-bool checkFileOpen(std::ifstream& file) {
-	if (file.fail()) {
-		cout << "파일 없음" << endl;
-		return false;
-	}
-	else return true;
-}
+
 int main() {
-	string password;
-	string name;
-	string number;
-	cout << "이름을 입력하세요 :";
+	string name, password, tel;
+
+	// 사용자에게 "이름을 입력하세요: "라는 메세지를 출력한 뒤 이름 입력받기
+	cout << "이름을 입력하세요: ";
 	cin >> name;
-	cout << "비밀번호를 입력하세요 :";
+
+	// 사용자에게 "비밀번호를 입력하세요: "라는 메세지를 출력한 뒤 비번 입력 받기
+	cout << "비밀번호를 입력하세요: ";
 	cin >> password;
+
+	ifstream inputFile("member.txt");
+	// 파일이 정상적으로 열렸는지 확인
+	if (inputFile.fail()) {
+		cout << "파일 없음" << endl;
+		return -1; // main함수 종료
+	}
+
+	// member.txt. 에서 한 줄씩 "이름"과 "비밀번호"를 검사하여 로그인 성공시 "로그인 성공" 실패시 "로그인 실패" 출력
 	string login = name + " " + password;
-	std::ifstream file_r("member.txt");
-	if (!checkFileOpen(file_r)) return -1;
-	std::string line, file_content = "";
-	while (std::getline(file_r, line)) {
-		// file_content = file_content + line + "\n";
-		if (line == login) {
+	string line, file_content = "";
+	while (getline(inputFile, line)) {
+		if (login == line) {
 			cout << "로그인 성공" << endl;
 			break;
 		}
 	}
-	if (line != login) {
+	if (login != line) {
 		cout << "로그인 실패" << endl;
-		return -1;
+		return -2;
 	}
-	cout << "전화번호를 입력해주세요(010-OOOO-OOOO) :";
-	cin >> number;
-	string tel = name + " " + number;
-	int count = 1;
-	std::ifstream file_m("member_tel.txt");
-	while (std::getline(file_m, line)) {
+
+	cout << "전화번호를 입력하세요: ";
+	cin >> tel;
+
+	// member_tel.txt 파일에서 전화번호가 이미 저장되어 있는지 확인
+	//bool found = false;
+	int found = 1;
+	ifstream telInputFile("member_tel.txt");
+	// 파일이 정상적으로 열렸는지 확인
+	if (telInputFile.fail()) {
+		cout << "파일 없음" << endl;
+		return -1; // main함수 종료
+	}
+
+
+	//string fileName, filePassword, fileTel;
+	//while (telInputFile >> fileName >> fileTel) {
+	//	if (name == fileName) {
+	//		found = true;
+	//		break;
+	//	}
+	//}
+
+	// 전화번호가 이미 저장되어 있으면 기존 전화번호 수정
+	// 아니면, 새로운 전화번호 저장
+	string number = name + " " + tel;
+	ifstream telInputFile2("member_tel.txt");
+	while (getline(telInputFile2, line)) {
 		if (line.substr(0, name.size()) == name) {
-			line = tel;
-			count--;
+			line = number;
+			found = false;
 		}
 		file_content += line + "\n";
 	}
-	if (count) {
-		std::ofstream file("member_tel.txt", std::ios::app);
-		if (file.fail()) {
+	if (found) {
+		ofstream telOutputFile("member_tel.txt", ios::app); // ios::app 이어쓰기
+		if (telOutputFile.fail()) {
 			cout << "파일 없음" << endl;
 			return -1;
 		}
-		file << tel << "\n";
+		telOutputFile << number << "\n";
 	}
 	else {
-		std::ofstream file("member_tel.txt");
-		if (file.fail()) {
+		ofstream telOutputFile("member_tel.txt", ios::app); // ios::app 이어쓰기
+		if (telOutputFile.fail()) {
 			cout << "파일 없음" << endl;
 			return -1;
 		}
-		file << file_content;
+		telOutputFile << file_content;
 	}
-	return 0;
 
+	return 0;
 }
