03aee46d695ba7016048c51431ab762b3b872035
gnaYnahC <acy7454@office.skhu.ac.kr>
2023-02-24 02:05:46 +0900

    공룡 게임 마무리

--- Project_DinoGame/Project_DinoGame/DinoGame.cpp
+++ Project_DinoGame/Project_DinoGame/DinoGame.cpp
@@ -5,84 +5,107 @@ using namespace sf;
 #define width 1000
 #define height 300
 
+// 구조체, Position 정의
 struct Position {
-  int x;
-  int y;
+  int x, y;
 };
 
-// 충돌
-bool isCollision(const Position& dinoPos, const Position& treePos,
-                 const Texture& dinoTexture, const Texture& treeTexture) {
-  const int dinoWidth = dinoTexture.getSize().x;
-  const int dinoHeight = dinoTexture.getSize().y;
-  const int treeWidth = treeTexture.getSize().x;
-  const int treeHeight = treeTexture.getSize().y;
+// 충돌 판정
+// Position과 Texture 객체를 참조자로 선언, 함수 호출 시 해당 객체를 직접 참조
+bool isCollision(const Position& DinoPos, const Position& TreePos,
+                 const Texture& DinoTexture, const Texture& TreeTexture) {
+  // Dino와 Tree의 너비와 높이를 텍스처의 크기 구함.
+  // DinoWidth, DinoHeight, TreeWidth,TreeHeight 변수에 저장
+  const int DinoWidth = DinoTexture.getSize().x;
+  const int DinoHeight = DinoTexture.getSize().y;
+  const int TreeWidth = TreeTexture.getSize().x;
+  const int TreeHeight = TreeTexture.getSize().y;
 
-  const int dinoLeft = dinoPos.x;
-  const int dinoRight = dinoPos.x + dinoWidth;
-  const int dinoTop = dinoPos.y;
-  const int dinoBottom = dinoPos.y + dinoHeight;
+  // Dino와 Tree의 왼쪽, 오른쪽, 위, 아래 좌표
+  // DinoLeft는 Dino의 왼쪽 좌표(DinoPos.x)
+  // DinoRight는 Dino의 오른쪽 좌표(DinoPos.x + DinoWidth)
+  // DinoTop은 Dino의 위쪽 좌표(DinoPos.y)
+  // DinoBottom은 Dino의 아래쪽 좌표(DinoPos.y + DinoHeight)
+  const int DinoLeft = DinoPos.x;
+  const int DinoRight = DinoPos.x + DinoWidth;
+  const int DinoTop = DinoPos.y;
+  const int DinoBottom = DinoPos.y + DinoHeight;
 
-  const int treeLeft = treePos.x;
-  const int treeRight = treePos.x + treeWidth;
-  const int treeTop = treePos.y;
-  const int treeBottom = treePos.y + treeHeight;
+  // 위와 같은 방식으로 변수에 저장
+  const int TreeLeft = TreePos.x;
+  const int TreeRight = TreePos.x + TreeWidth;
+  const int TreeTop = TreePos.y;
+  const int TreeBottom = TreePos.y + TreeHeight;
 
-  return (dinoRight > treeLeft) && (dinoLeft < treeRight) &&
-         (dinoBottom > treeTop) && (dinoTop < treeBottom);
+  // DinoRight가 TreeLeft보다 크고, DinoLeft가 TreeRight보다 작으며,
+  // DinoBottom이 TreeTop보다 크고, DinoTop이 TreeBottom보다 작은 경우
+  // true반환
+  return (DinoRight > TreeLeft) && (DinoLeft < TreeRight) &&
+         (DinoBottom > TreeTop) && (DinoTop < TreeBottom);
 }
 
 int main() {
   int score = 0;
-  // SFML 윈도우을 열어고 title을 설정
+  // SFML 윈도우을 열고 title을 설정
   RenderWindow window(VideoMode(width, height), "DinoSaur Game!!");
-  // 프레임 설정
+
+  // 프레임 설정 - 프레임 제한 속도 == 초당 60프레임
   window.setFramerateLimit(60);
 
-  // 이미지 불러오기
-  Texture t1, t2, t3, t4, t5;
-  t1.loadFromFile("Dino_images/dino1.png");  // 공
-  t2.loadFromFile("Dino_images/dino2.png");  // 룡
-  t3.loadFromFile("Dino_images/tree.png");   // 나무
-  t4.loadFromFile("Dino_images/cloud.png");   // 구름
+  // 캐릭터 또는 배경 이미지를 읽어서 화면에 출력하는 Texture클래스
+  Texture t1, t2, t3, t4, t5;                 // 이미지 로드
+  t1.loadFromFile("Dino_images/dino1.png");   // 공
+  t2.loadFromFile("Dino_images/dino2.png");   // 룡
+  t3.loadFromFile("Dino_images/tree.png");    // 나무
+  t4.loadFromFile("Dino_images/cloud1.png");  // 구름1
+  t5.loadFromFile("Dino_images/cloud2.png");  // 구름2
 
-  // 캐릭터 또는 배경 이미지를 읽어서 화면에 출력하는 Sprite클래스
-  // 배열에 공룡 저장 선언, 나무 선언
-  Sprite DinoArr[2], Tree(t3), Cloud(t4);
+  // Sprite 생성
+  // Sprite - 이미지를 window에 그릴 수 있는 개체
+  Sprite DinoArr[2], Tree(t3), Cloud1(t4), Cloud2(t5);
   DinoArr[0] = Sprite(t1);
   DinoArr[1] = Sprite(t2);
 
   // 공룡 바닥 설정
-  static const int DINO_Y_BOTTOM = height - t1.getSize().y;
+  // static - 정적 변수를 선언하기 위한 키워드 (프로그램 실행 시 메모리 공간
+  // 할당, 종료되기 전까지 유지) const - 상수 선언을 위한 키워드 (해당 변수는 값
+  // 변경 불가능)
+  static const int DINO_BOTTOM = height - t1.getSize().y;
   Position DinoPos;
   DinoPos.x = 30;
-  DinoPos.y = DINO_Y_BOTTOM;
+  DinoPos.y = DINO_BOTTOM;
 
   // 나무 바닥 설정
-  static const int TREE_Y_BOTTOM = height - t3.getSize().y;
+  static const int TREE_BOTTOM = height - t3.getSize().y;
   Position TreePos;
   TreePos.x = width - 20;
-  TreePos.y = TREE_Y_BOTTOM;
+  TreePos.y = TREE_BOTTOM;
 
   // 구름 위치 설정
-  static const int CLOUD_Y_TOP = height - 280;
-  Position CloudPos;
-  CloudPos.x = width;
-  CloudPos.y = CLOUD_Y_TOP;
+  // cloud1과 cloud2가 이어지도록 위치 설정
+  static const int CLOUD_TOP = height - t4.getSize().y * 3;
+  Position CloudPos1;
+  CloudPos1.x = 0;
+  CloudPos1.y = CLOUD_TOP;
 
-  // 프레임에 따라 왼발||오른발 해주기 위한 변수 네개
-  int index = 0;  // 왼발 오른발 인덱스
-  float frame = 0.f;
-  float frameSpeed = 0.4f;
-  const int changeCount = 5;  // 몇 프레임에 체인지 할지
+  Position CloudPos2;
+  CloudPos2.x = t4.getSize().x;
+  CloudPos2.y = CLOUD_TOP;
+
+  // 캐릭터 애니메이션을 위한 변수
+  int footShapeIndex = 0;  // 캐릭터 발 모양을 결정
+  int currentFrame = 0;    // 현재 프레임 수 저장
+  float frameDelay = 0.4f;  // 한 프레임당 대기 시간
+             // 실수형으로 선언되어 있지 않으면 소수점 이하의 값이 버려지게 됨
+  const int totalFrames = 5;  // 발 모양을 변경할 총 프레임 수
 
   // 공룡 점프
-  const int gravity = 7;   // 점프시 중력
-  bool isJumping = false;  // 점프중
-  bool isBottom = true;    // 걷는중
+  const int JumpPower = 9;  // 점프력
+  bool isJumping = false;   // 점프중
+  bool isBottom = true;     // 걷는중
 
   // 나무 속도
-  const int TreeSpeed = 5;
+  const int TreeSpeed = 10;
 
   // 구름 속도
   const int CloudSpeed = 3;
@@ -96,7 +119,7 @@ int main() {
       }
     }
     // 공룡 점프시켜주기
-    if (Keyboard::isKeyPressed(Keyboard::Space)) {  // Space 클릭 시
+    if (Keyboard::isKeyPressed(Keyboard::Space)) {  // Space 클릭
       if (isBottom && !isJumping) {  // 공룡 걷고있고, 점프 안하고 있을 때
         isJumping = true;
         isBottom = false;  // 상태 변경
@@ -104,28 +127,29 @@ int main() {
     }
     // 점프
     if (isJumping) {
-      DinoPos.y -= gravity;  // 점프중엔 y위치에서 gravity를 뺌 (그럼 올라감)
+      DinoPos.y -=
+          JumpPower;  // 점프중엔 y위치에서 JumpPower를 뺌 (그럼 올라감)
     } else {
-      DinoPos.y += gravity;  // 고점에서 중력을 더하여 아래로
+      DinoPos.y += JumpPower;  // 고점에서 점프파워를 더하여 아래로
     }
     // 공룡의 위치 한계점 지정
-    if (DinoPos.y >= DINO_Y_BOTTOM)  // 바닥에서 지하로 가지 않도록 설정
+    if (DinoPos.y >= DINO_BOTTOM)  // 바닥에서 지하로 가지 않도록 설정
     {
-      DinoPos.y = DINO_Y_BOTTOM;
+      DinoPos.y = DINO_BOTTOM;
       isBottom = true;
     }
-    if (DinoPos.y <= DINO_Y_BOTTOM - 100)  // 점프해서 하늘에 부딪히지 않게 설정
+    if (DinoPos.y <= DINO_BOTTOM - 100)  // 점프해서 하늘에 부딪히지 않게 설정
     {
       isJumping = false;
     }
 
     //  공룡 아장아장
-    frame += frameSpeed;
-    if (frame > changeCount) {
-      frame -= changeCount;
-      ++index;
-      if (index >= 2) {
-        index = 0;
+    currentFrame += frameDelay;  // 현재 시간에서 마지막 업데이트된 시간을 더한 값
+    if (currentFrame > totalFrames) {  // 한 번의 애니메이션이 완료
+      currentFrame -= totalFrames;  // 현재 프레임을 다시 0부터 시작
+      ++footShapeIndex;             // 1씩 증가
+      if (footShapeIndex >= 2) {  // 2 이상인 경우 다시 0으로 초기화
+        footShapeIndex = 0;
       }
     }
 
@@ -137,21 +161,28 @@ int main() {
       TreePos.x -= TreeSpeed;
     }
 
-    // 구름 이동
-    if (CloudPos.x <= -800) {
-      CloudPos.x = width;
+    // 구름을 이동하고 화면 밖으로 나가면 다시 처음으로 돌림
+    if (CloudPos1.x <= -1000) {
+      CloudPos1.x = width;
     } else {
-      CloudPos.x -= CloudSpeed;
+      CloudPos1.x -= CloudSpeed;
+    }
+
+    if (CloudPos2.x <= -1000) {
+      CloudPos2.x = width + CloudPos1.x;
+    } else {
+      CloudPos2.x -= CloudSpeed;
     }
 
     // 공룡 위치
-    DinoArr[index].setPosition(DinoPos.x, DinoPos.y);
+    DinoArr[footShapeIndex].setPosition(DinoPos.x, DinoPos.y);
 
     // 나무 위치
     Tree.setPosition(TreePos.x, TreePos.y);
 
     // 구름 위치
-    Cloud.setPosition(CloudPos.x, CloudPos.y);
+    Cloud1.setPosition(CloudPos1.x, CloudPos1.y);
+    Cloud2.setPosition(CloudPos2.x, CloudPos2.y);
 
     // 충돌 시 게임 오버 처리
     if (isCollision(DinoPos, TreePos, t1, t3)) {
@@ -161,14 +192,14 @@ int main() {
     }
 
     // 윈도우
-    window.clear(Color::Cyan);    // 배경
-    window.draw(DinoArr[index]);  // 공룡
-    window.draw(Tree);            // 나무
-    window.draw(Cloud);           // 구름
+    window.clear(Color::Cyan);             // 배경
+    window.draw(DinoArr[footShapeIndex]);  // 공룡
+    window.draw(Tree);                     // 나무
+    window.draw(Cloud1);                   // 구름1
+    window.draw(Cloud2);                   // 구름2
 
     // 윈도우 디스플레이 출력
     window.display();
   }
-
   return 0;
 }
